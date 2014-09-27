91a0f1b9a90a257ee834b1c1e7a46f5117521510
Sandeep Panda <sandeep@thexprs.com>
2014-09-27 21:19:35 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -16,64 +16,64 @@ var app = express();
 //connect to our database
 var mongoose = require('mongoose');
 
-var dbName='blogger';
+var dbName='reactApp';
 
 var connectionString='mongodb://localhost:27017/'+dbName;
 
-mongoose.connect(connectionString);
+mongoose.connect(connectionString,function(){
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(__dirname + '/public/favicon.ico'));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api',posts);
+    app.use('/api',posts);
 
-app.get('*',function(req,res){
-    var path = url.parse(req.url).pathname;
-    ReactAsync.renderComponentToStringWithAsyncState(App({path:path}),function(err, markup) {
-        res.send('<!DOCTYPE html>'+markup);
+    app.get('*',function(req,res){
+        var path = url.parse(req.url).pathname;
+        ReactAsync.renderComponentToStringWithAsyncState(App({path:path}),function(err, markup) {
+            res.send('<!DOCTYPE html>'+markup);
+        });
     });
-});
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-// error handlers
+    // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: {}
         });
     });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
 });
-
 
 module.exports = app;
--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,32 @@ var Post=require('../models/post');
 var express=require('express');
 var router=express.Router();
 
+//Let's add a few sample posts.
+
+var postsArr=[
+    {
+        "title": "What is Lorem Ipsum",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        "title": "Why use Lorem Ipsum",
+        "content": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    },
+    {
+        "title": "Sample Lorem Ipsum",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget pellentesque neque, ac pellentesque elit. Integer at nisi non elit vulputate feugiat. Proin finibus tortor in felis lacinia luctus. Donec at venenatis augue. Sed vulputate hendrerit feugiat. Praesent semper ex a nulla viverra, ac finibus diam dapibus. In vel lacinia nulla, in hendrerit nisl. Morbi ac metus metus."
+    }
+]
+
+Post.find(function(err,posts){
+    if(posts && posts.length===0){
+        for(var i=0;i<postsArr.length;i++){
+            var postInstance=new Post(postsArr[i]);
+            postInstance.save();
+        }
+    }
+});
+
 router.route('/posts')
     .get(function(req,res){
         Post.find(function(err,posts){
